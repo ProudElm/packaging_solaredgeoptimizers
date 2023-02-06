21512b7818ee75090592d133e66c33db86dfe87e
ProudElm <job_sande@yahoo.co.uk>
2023-02-06 17:31:15 +0100

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-py -m build
-
-py -m twine upload --repository pypi dist/* --username=__token__ --password=pypi-AgEIcHlwaS5vcmcCJDVjZjkxNTkyLTg5ZDQtNDdjYi04Nzc3LThkMWE5NDUyZWIyYgACKlszLCIzYjljZGU3Mi01Y2VmLTRiMWQtYjc2My00OTQzZWZhMDBiNWYiXQAABiCtXzTux3scyhL1lzYyHyg4g-H7NaVdb78cB5k6euiqUg
-
-pause
